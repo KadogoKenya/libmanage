b7cb48ead0a858da9c66da5a9ebc1fda423cc495
Kate <you@example.com>
2022-03-08 10:59:25 +0300

    initial commit

--- library/settings.py
+++ library/settings.py
@@ -59,7 +59,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&)7a+yt%r!yicmo&*98y5#h@hmucer-2h_ojn)&cxa5$00&-*%'
+# SECRET_KEY = 'django-insecure-&)7a+yt%r!yicmo&*98y5#h@hmucer-2h_ojn)&cxa5$00&-*%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
